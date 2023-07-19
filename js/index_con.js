c26989b7a135b1b92df5e6e4dec82c6c50ae93b0
hayko003 <133478208+hayko003@users.noreply.github.com>
2023-07-19 10:51:59 +0300

    Add files via upload

--- js/index_con.js
+++ js/index_con.js
@@ -1,22 +1,12 @@
-function myFn(a, b) {
-  let c;
-  a = a + 1;
-  c = a + b;
-  return c;
-}
-
-console.log.myFn();
-
 const personOne = {
-  name:"Bob",
+  name: "Bob",
   age: 21,
+};
+
+function callMyName() {
+  console.log("Arto");
 }
 
-function increasePersonAge(person) {
-  const updatedPerson = Object.assign({}, person)
-  updatedPerson.age += 1
-  return updatedPerson
-}
+console.log("Start");
 
-const updatedPersonOne = increasePersonAge(PersonOne)
-console.log(personOne.age)
+setTimeout(callMyName, 2000);
